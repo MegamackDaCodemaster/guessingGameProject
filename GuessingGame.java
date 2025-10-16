fa7d77653d00b97d65c30d9bd4d3d4f0878c6816
MegamackDaCodemaster <ac75115@skola.goteborg.se>
2025-10-16 11:50:16 +0200

    Added 2 int variables from and to in GuessingGame.java

--- GuessingGame.java
+++ GuessingGame.java
@@ -2,6 +2,8 @@ public class GuessingGame {
 
 	public static void main(String[] args) {
 
-		Guesser guesser = new Guesser();
+		int from;
+		int to;
+		Guesser guesser = new Guesser(from, to);
 	}
 }
