960b870d94c9e36d1981d9908ad0e41857b99b3f
MegamackDaCodemaster <ac75115@skola.goteborg.se>
2025-10-22 16:43:59 +0200

    Revert "Added main method and ArrayList with players to 'Players.java'"
    This reverts commit 4b016c73dd1f9f8f4abf39e87de5422c280a894c.

--- Players.java
+++ Players.java
@@ -1,13 +1,3 @@
-
-import java.util.*;
-
 public class Players {
 
-	public static void main(String[] args) {
-
-		ArrayList<String> players = new ArrayList<>();
-
-		players.add("Bosse");
-		players.add("Mackan");
-	}
 }
