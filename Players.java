682e93233c1b239d02e52ad1cb7742476d01a27b
MegamackDaCodemaster <ac75115@skola.goteborg.se>
2025-10-22 14:30:57 +0200

    Added the 'Players' class to the project!

---
+++ Players.java
@@ -0,0 +1,13 @@
+
+import java.util.*; 
+
+public class Players {
+
+	public static void main(String[] args) {
+
+		ArrayList<String> players = new ArrayList<>();
+
+		players.add("Bosse");
+		players.add("Mackan");
+	}
+}
