6465046e805c1bdfff53b67c93cf56eb337bf9ae
MegamackDaCodemaster <ac75115@skola.goteborg.se>
2025-10-23 15:06:04 +0200

    The script 'run_game.sh' was added

---
+++ run_game.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+name="Marcus_Björkman"
+echo "$name""s program"
+base_dir="$(pwd)"
+mkdir "$base_dir/$name"_labb
+cp "$base_dir"/GuessingGame.java "$base_dir"/Guesser.java "$base_dir/$name"_labb
+cd ..
+cd "$base_dir/$name"_labb
+echo "Running game from $(pwd)"
+echo "compiling..."
+javac GuessingGame.java Guesser.java
+cd ..
+echo "running..."
+java GuessingGame
+echo "Done!"
+cd "Marcus_Björkman_labb"
+echo "Removing class files..."
+rm GuessingGame.class Guesser.class
+ls
+
+
